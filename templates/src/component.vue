5fa90c1e61c9d19fd29c61976a91a750dd278c2b
levy <897895407@qq.com>
2018-06-11 02:21:22 +0800

    remove sample code in component.vue, and adjust code like fem team way

--- templates/src/component.vue
+++ templates/src/component.vue
@@ -1,35 +1,23 @@
+<template>
+  <div class="{{componentName}}">
+  </div>
+</template>
+
 <script>
 export default {
-    name: '{{componentNamePascal}}', // vue component name
+    name: '{{componentNamePascal}}',
+    props: {
+
+    },
     data() {
         return {
-            counter: 5,
-            initCounter: 5,
-        };
-    },
+        }
+    }
 }
 </script>
 
-<template>
-    <div class="{{componentName}}">
-        <p>The counter is set to <b>{{counter}}</b>.</p>
-        <button @click="counter += 1">Click +1</button>
-        <button @click="counter -= 1">Click -1</button>
-        <button @click="counter = initCounter">Reset</button>
-    </div>
-</template>
+<style lang="stylus">
+.{{componentName}} {
 
-<style scoped>
-    .{{componentName}} {
-        display: block;
-        width: 400px;
-        margin: 25px auto;
-        border: 1px solid #ccc;
-        background: #eaeaea;
-        text-align: center;
-        padding: 25px;
-    }
-    .{{componentName}} p {
-        margin: 0 0 1em;
-    }
+}
 </style>
